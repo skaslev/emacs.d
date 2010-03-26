8bc8021bb95cf76c6df19da189235a631b5a92d4
Slavomir Kaslev <slavomir.kaslev@gmail.com>
2010-03-26 13:42:09 +0200

    Added swap-windows (kbd C-c s)

--- init.el
+++ init.el
@@ -27,6 +27,28 @@
 (global-set-key (kbd "C-=") 'text-scale-increase)
 (global-set-key (kbd "C--") 'text-scale-decrease)
 
+(global-set-key (kbd "C-c n") 'linum-mode)
+(global-set-key (kbd "C-c w") 'whitespace-mode)
+
+(defun swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((/= (count-windows) 2)
+         (message "You need exactly 2 windows to do this."))
+        (t (let* ((w1 (first (window-list)))
+                  (w2 (second (window-list)))
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+                  (s1 (window-start w1))
+                  (s2 (window-start w2)))
+             (set-window-buffer w1 b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1))))
+  (other-window 1))
+
+(global-set-key (kbd "C-c s") 'swap-windows)
+
 (defun toggle-fullscreen ()
   (interactive)
   (set-frame-parameter nil 'fullscreen
@@ -77,9 +99,6 @@
 
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward-angle-brackets)
-
-(global-set-key (kbd "C-c n") 'linum-mode)
-(global-set-key (kbd "C-c w") 'whitespace-mode)
 
 (require 'pager)
 (global-set-key (kbd "C-v") 'pager-page-down)
